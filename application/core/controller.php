294ff2df5559f21860c574242dd444b9e065fef0
varunsinghal <varunsinghal15@gmail.com>
2017-06-18 00:27:39 +0530

    removed charset from connection string

--- application/core/controller.php
+++ application/core/controller.php
@@ -24,7 +24,7 @@ class Controller
     private function openDatabaseConnection()
     {
         $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING);
-        $this->db = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET, DB_USER, DB_PASS, $options);
+        $this->db = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME , DB_USER, DB_PASS, $options);
     }
 
     public function protect($input)
